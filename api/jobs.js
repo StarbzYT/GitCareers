8dc2b1d53f3b8f2cc6e5820195fe9c4e57a9010e
Charlie Wager <charlie.wagerr@gmail.com>
2022-01-15 18:04:22 -0800

    fully working joblist

--- api/jobs.js
+++ api/jobs.js
@@ -13,5 +13,5 @@ class JobList{
         console.log(list);
     }
 }
-const newList = new JobList("http://api.adzuna.com/v1/api/jobs/gb/search/1?app_id=%7B1a947cf7&app_key=480a50fa2ccc0bb63d1b78f7072d2fa8&results_per_page=9&what=");
+const newList = new JobList("https://api.adzuna.com/v1/api/jobs/gb/search/1?app_id=1a947cf7&app_key=480a50fa2ccc0bb63d1b78f7072d2fa8&results_per_page=9&what=");
 newList.getJobList("javascript");
