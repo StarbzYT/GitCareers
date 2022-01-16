4a70d5f646a05a861ddbec5261ddc22cdae54e7e
Charlie Wager <charlie.wagerr@gmail.com>
2022-01-15 17:45:20 -0800

    sorta working api

--- api/jobs.js
+++ api/jobs.js
@@ -4,5 +4,14 @@ class JobList{
         this.url = url;
     }
 
-    
+    async getJobList(language){
+
+        const request = `${this.url}${language}%20developer&content-type=application/json`;
+        const response = await fetch(request);
+        const list = await response.json();
+
+        console.log(list);
+    }
 }
+const newList = new JobList("http://api.adzuna.com/v1/api/jobs/gb/search/1?app_id=%7B1a947cf7&app_key=480a50fa2ccc0bb63d1b78f7072d2fa8&results_per_page=9&what=");
+newList.getJobList("javascript");
